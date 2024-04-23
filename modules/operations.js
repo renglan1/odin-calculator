9cf54e6286cf4fc39b9d4e3d8135a2800617d4bf
Rob England <robenng@icloud.com>
2024-04-23 14:40:17 -0300

    Calculator style complete

---
+++ modules/operations.js
@@ -0,0 +1,17 @@
+function add(opOne, opTwo) {
+  return opOne + opTwo;
+}
+
+function subtract(opOne, opTwo) {
+  return opOne - opTwo;
+}
+
+function multiply(opOne, opTwo) {
+  return opOne*opTwo;
+}
+
+function divide(numerator, denominator) {
+  return numerator / denominator;
+}
+
+export { add, subtract, multiply, divide };
